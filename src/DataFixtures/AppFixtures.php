f632e2e0ea036b994ba1ae21d86ff7305ed81844
Antrâxx <h.julien62@gmail.com>
2022-09-01 14:27:05 +0200

    Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,7 +3,6 @@
 namespace App\DataFixtures;
 
 use App\Entity\Event;
-use Date;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -14,17 +13,20 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-
-        $date = new Date();
-        $datetime = new DateTime();
+        $time1 = new DateTime('2022-09-01 13:00:00');
+        $time2 = new DateTime('2022-09-02 21:00:00');
+        $time3 = new DateTime('2022-09-02 23:00:00');
+        $time4 = new DateTime('2022-09-01 14:00:00');
+        $time5 = new DateTime('2022-09-04 17:00:00');
+        $time6 = new DateTime('2022-09-05 23:30:00');
 
         $event = new Event();
         $event->setName('Concert Alborosie');
         $event->setDescription('Un concert de reaggae avec Alborosie en vedette.');
         $event->setPrice(35);
-        $event->setCreatedAt($date('2022-08-23'));
-        $event->setStart($datetime('2022-09-02 21:00:00'));
-        $event->setEnd($datetime('2022-09-02 23:00:00'));
+        $event->setCreated($time1);
+        $event->setStart($time2);
+        $event->setEnd($time3);
         $event->setImage('null');
         $manager->persist($event);
 
@@ -32,9 +34,9 @@ class AppFixtures extends Fixture
         $event->setName('Festival de l\'été');
         $event->setDescription('Mickey3D, Pierpoljak, Vianney, Bob Sinclar');
         $event->setPrice(45);
-        $event->setCreatedAt($date('2022-08-22'));
-        $event->setStart($datetime('2022-09-04 17:00:00'));
-        $event->setEnd($datetime('2022-09-05 23:30:00'));
+        $event->setCreated($time4);
+        $event->setStart($time5);
+        $event->setEnd($time6);
         $event->setImage('null');
         $manager->persist($event);
 
